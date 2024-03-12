d1611cb117da3c46d36b3e0748c6c1a14b70c41f
PHase00 <113923045+PHase00@users.noreply.github.com>
2024-03-12 23:06:33 +0700

    Change for Retrieving Single Models Practice

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,21 +10,32 @@ class UserController extends Controller
 {
 
     //Pertemuan 3
+    //Pertemuan 4
     function index (){
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345'),
-        ];
-        UserModel::create($data);
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345'),
+        // ];
+        // UserModel::create($data);
 
         // $data = [
         //     'nama' => 'Pelanggan Pertama'
         // ];
         // UserModel::where('username', 'customer-1')->update($data);
 
-        $user = UserModel::all();
+        // $user = UserModel::all();
+        
+        // $user = UserModel::find(1);
+
+        // $user = UserModel::where('level_id', 1)->first();
+
+        // $user = UserModel::firstWhere('level_id', 1);
+
+        $user = UserModel::findOr(20, ['username', 'nama'], function(){
+            abort(404);
+        });
         return view('user', ['data' => $user]);
     }
 
